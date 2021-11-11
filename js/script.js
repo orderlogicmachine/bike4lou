0ebfe3309d4d0bae812cddc29cd02c7da2a571c4
JD D'Annunzio <jddannunzio@icloud.com>
2021-11-11 14:13:35 -0500

    Added initial email validation JS. Not currently working in Chrome simulator

--- js/script.js
+++ js/script.js
@@ -1,9 +1,30 @@
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-function myFunction() {
-    var x = document.getElementById("navLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
+function myFunction()
+{
+  var x = document.getElementById("navLinks");
+  if (x.style.display === "block")
+  {
+    x.style.display = "none";
   }
+  else
+  {
+    x.style.display = "block";
+  }
+}
+
+function ValidateEmail(inputText)
+{
+  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (inputText.value.match(mailformat))
+  {
+    alert("Thank you for joining Bike 4 Louisville!");
+    document.signUp.mail.focus();
+    return true;
+  }
+  else
+  {
+    alert("Please double-check your email address.");
+    document.signUp.mail.focus();
+    return false;
+  }
+}
